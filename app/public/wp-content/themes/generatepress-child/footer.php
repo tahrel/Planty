7b66bf682a3b6964376764d2273ef820d37fad54
tahrel <tahrel44@gmail.com>
2024-09-27 11:31:12 +0200

    Ajout Css

--- app/public/wp-content/themes/generatepress-child/footer.php
+++ app/public/wp-content/themes/generatepress-child/footer.php
@@ -13,13 +13,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 </div>
 <footer class="site-footer">
     <div class="footer-container">
+</footer>
+
     <?php
 if (is_page('accueil')) {
     // Contenu spécifique pour la page "about"
-    echo '<footer>Contenu du footer pour la page About</footer>';
+    echo '<footer class="custom-footer">
+    <div class="canette height-2"></div>
+    <div class="canette height-1"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-3"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-1"></div>
+    <div class="canette height-3"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-1"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-3"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-1"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-3"></div>
+    <div class="canette height-2"></div>
+</footer>
+';
 } elseif (is_page('nous-rencontrer')) {
     // Contenu spécifique pour la page "contact"
-    echo '<footer>Contenu du footer pour la page Contact</footer>';
+    echo '<footer class="custom-footer">
+    <div class="canette height-2"></div>
+    <div class="canette height-1"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-3"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-1"></div>
+    <div class="canette height-3"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-1"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-3"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-1"></div>
+    <div class="canette height-2"></div>
+    <div class="canette height-3"></div>
+    <div class="canette height-2"></div>
+</footer>';
 }
 ?>
 <a href="<?php echo esc_url( home_url( '/mentions-legales' ) ); ?>">Mentions légales</a>
